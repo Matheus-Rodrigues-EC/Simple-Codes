6b169f0c7204795d12ee69bbc6df07731f1ca855
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2021-06-25 01:28:45 -0300

    Primeiro código da calculadora
    Código em C++ contendo calculadora simples com as 4 operações básicas.

---
+++ Calculadora_Simples.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	
+	int A, B;
+	char C;
+	
+	cout << "Digite o primeiro numero: ";
+	cin >> A;
+	cout << "Digite a operacao desejada: ";
+	cin >> C;
+	cout << "Digite o segundo numero: ";
+	cin >> B;
+	
+	if(C == '+') cout << "O resultado eh: " << A + B;
+	if(C == '-') cout << "O resultado eh: "  << A - B;
+	if(C == '*') cout << "O resultado eh: "  << A * B;
+	if(C == '/' and B!=0){
+		cout << "O resultado eh: "  << A / B;
+	}else if(C == '/' and B == 0){
+		cout << "Indeterminacao matematica.";
+	}
+		
+	
+	
+	
+	return 0;
+}
